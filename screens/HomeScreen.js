285617d2fab070fa051eb5d63b0c4f6c28fd1975
Patrice <monat.patrice@gmail.com>
2025-03-03 16:06:52 +0100

    pulse home page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -22,60 +22,143 @@ export default function HomeScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      {/* <LinearGradient
+      <View style={styles.containerImageSign}>
+      <LinearGradient
         // Background Linear Gradient
-        colors={['transparent', '#A3E2F8']}
+        colors={['#FFFFFF', '#A3E2F8']}
+        start={{ x: 0.5, y: 0.3}}
+        end={{ x: 0.5, y: 0.9 }}
         style={styles.background}
-      /> */}
-      {/* <Image source={require('../assets/logo.png')} size={500}/> */}
-      {/* <Image
-              style={styles.logo}
-              source={require("../assets/icon.png")}
-              size={25}
-            /> */}
-      <Text>Vous avez déjà un compte ?</Text>
-      <TouchableOpacity
-        onPress={() => handleSubmit()}
-        style={styles.button}
-        activeOpacity={0.8}
       >
-        <Text style={styles.textButton}>Sign in</Text>
-      </TouchableOpacity>
-      <Text>Inscrivez-vous</Text>
-      <TouchableOpacity
-        onPress={() => handleSubmit()}
-        style={styles.button}
-        activeOpacity={0.8}
-      >
-        <Text style={styles.textButton}>Sign up</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => handleSubmit()}
-        style={styles.button}
-        activeOpacity={0.8}
-      >
-        <Text style={styles.textButton}>Continuer sans compte</Text>
-      </TouchableOpacity>
-      <Text style={styles.textFooter}>@Pulse | 2025</Text>
-      <Text style={styles.textFooter}>Besoin d'aide ? Nous contacter</Text>
+        <Image style={styles.logo} source={require('../assets/Logo.png')} />
+        <View style={styles.containerSign}>
+        <Text style={styles.text}>Vous avez déjà un compte ?</Text>
+        <TouchableOpacity
+          onPress={() => handleSubmit()}
+          style={styles.buttonSignin}
+          activeOpacity={0.8}
+        >
+          <Text style={styles.textButton}>Sign in</Text>
+        </TouchableOpacity>
+        <View style={styles.line}></View>
+        <Text style={styles.text}>Inscrivez-vous</Text>
+        <TouchableOpacity
+          onPress={() => handleSubmit()}
+          style={styles.buttonSignup}
+          activeOpacity={0.8}
+        >
+          <Text style={styles.textButtonSignup}>Sign up</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => handleSubmit()}
+          style={styles.buttonContinue}
+          activeOpacity={0.8}
+        >
+          <Text style={styles.textButton}>Continuer sans compte</Text>
+        </TouchableOpacity>
+      </View>
+      </LinearGradient>
+      </View>
+      <View style={styles.containerTextFooter}>
+        <Text style={styles.textFooter}>@Pulse | 2025</Text>
+        <Text style={styles.textFooter}>Besoin d'aide ? Nous contacter</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderBottomRightRadius: 36,
+    borderBottomLeftRadius: 36,
+    paddingBottom: 50,
+    
+  },
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-between",
+    
+  },
+  containerImageSign: {
+    width: "100%",
+    height: "90%",
+    alignItems: "center",
+    borderBottomRightRadius: 36,
+    borderBottomLeftRadius: 36,
+  },
+  containerSign: {
+    alignItems: "center",
+    width: "80%",
   },
   logo: {
-    width: 500,
-    height: 500,
+    resizeMode: "contain",
+    width: 400,
+    height: 200,
+    marginTop: 150,
+    
   },
-  button: {
+  buttonSignup: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: 43,
+    borderWidth: 1,
+    borderColor: "#bbbbbb",
+    backgroundColor: "#FF6C02",
+    borderRadius: 8,
+  },
+  line: {
+    marginTop: 20,
+    width: "100%",
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+  },
+  buttonSignin: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: 43,
     backgroundColor: "red",
+    borderWidth: 1,
+    borderColor: "#bbbbbb",
+    backgroundColor: "white",
+    borderRadius: 8,
+  },
+  buttonContinue: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: 43,
+    marginTop: 80,
+    backgroundColor: "red",
+    borderWidth: 1,
+    borderColor: "#bbbbbb",
+    backgroundColor: "white",
+    borderRadius: 8,
+  },
+  text: {
+    marginTop: 20,
+    marginBottom: 10,
   },
   textButton: {
+    fontWeight: 900,
+    color: "black",
+  },
+  textButtonSignup: {
+    fontWeight: 900,
     color: "white",
+  },
+  containerTextFooter: {
+    alignItems: "center",
+    marginBottom: 25,
+  },
+  textFooter: {
+    fontSize: 10,
+    fontWeight: 900,
   },
 });
