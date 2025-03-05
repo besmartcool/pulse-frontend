b82a84139b530f5cb32ab01508b8781499f288ff
Maurice Tadonkeng <tadonkengmaurice@gmail.com>
2025-03-05 10:10:12 +0100

    export liste catégories

--- assets/categoriesList.js
+++ assets/categoriesList.js
@@ -1,3 +1,5 @@
 const categoriesList = [
     "Éducation", "Santé", "Environnement", "Sport", "Culture", "Technologie", "Humanitaire", "Socioculturel", "Politique", "Spiritualité", "Emploi & Développement local", "Amicales", "Solidarité", "Loisirs", "Réflexion", "Activités économiques", "Droits & Civisme", "Non renseigné", "Communication", "Interventions sociales", "Justice", "Logement", "Patrimoine", "Recherche", "Économie", "Sécurité", "Médico-social", "Services familiaux", "Tourisme",
 ]
+
+export default categoriesList;
