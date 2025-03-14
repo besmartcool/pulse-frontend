5e909dbb3ca2b5ac4ef5b879a057c6b967181359
Maurice Tadonkeng <tadonkengmaurice@gmail.com>
2025-03-14 14:22:04 +0100

    addresse backend déployé et url.js enlevé du gitignore

---
+++ assets/url.js
@@ -0,0 +1,4 @@
+// export const BACKEND_ADDRESS = "http://10.0.1.62:3000"
+export const BACKEND_ADDRESS = "https://pulse-backend-virid.vercel.app/"
+// export const BACKEND_ADDRESS = "http://192.168.1.169:3000"
+// export const BACKEND_ADDRESS = "http://192.168.62.103.3000"
